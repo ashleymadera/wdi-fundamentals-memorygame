cd4189379f074be75aaa1acaad1d633b74cd6e70
Ashley Madera <ashleymadera@Ashleys-MacBook-Pro.local>
2020-02-02 12:43:10 -0500

    memory game changes

---
+++ memory-game/js/main.js
@@ -0,0 +1,23 @@
+console.log("Up and running!");
+
+const cards=["queen", "queen", "king", "king"];
+const cardsInPlay=[];
+
+let cardOne= cards[0];
+let cardTwo= cards[2];
+let cardThree= cards[1];
+let cardFour= cards[3];
+
+cardsInPlay.push(cardOne);
+
+
+console.log("User flipped " + cardsInPlay[0]);
+
+cardsInPlay.push(cardTwo);
+
+
+console.log("User flipped " + cardsInPlay[1
+
+	if (cardsInPlay.length === 2){
+
+	}
