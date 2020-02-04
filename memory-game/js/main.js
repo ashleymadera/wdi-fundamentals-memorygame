8628233f386545b562154ec8abb776ebe9544b69
Ashley Madera <ashleymadera@Ashleys-MacBook-Pro.local>
2020-02-04 14:23:49 -0500

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -4,20 +4,27 @@ const cards=["queen", "queen", "king", "king"];
 const cardsInPlay=[];
 
 let cardOne= cards[0];
-let cardTwo= cards[2];
-let cardThree= cards[1];
-let cardFour= cards[3];
-
 cardsInPlay.push(cardOne);
-
-
 console.log("User flipped " + cardsInPlay[0]);
 
+let cardTwo= cards[2];
 cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
 
 
-console.log("User flipped " + cardsInPlay[1
+/*let cardThree= cards[3];
+cardsInPlay.push(cardThree);
+console.log("User flipped " + cardsInPlay[2]);*/
 
-	if (cardsInPlay.length === 2){
 
+let cardFour= cards[4];
+
+
+	if (cardsInPlay.length === 2)
+	{
+		if (cardsInPlay[0]===cardsInPlay[2])
+		alert(" You found a match!");
+		else 
+		alert("Sorry, try again!");
 	}
+	
