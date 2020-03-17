9acbf251f0b8d0f184cb59fad35793bc3acc760f
Ashley Madera <ashleymadera@Ashleys-MacBook-Pro.local>
2020-03-17 18:21:58 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,12 +1,33 @@
 
 
-let cards = ["queen","queen","king","king"];
+let cards = [
+{rank:"queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank:"queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank:"king",
+suit: "hearts",
+cardImage:"images/king-of-hearts.png"
+},
+{
+rank:"king",
+suit: "diamonds",
+cardImage:"images/king-of-diamonds.png"
+}
+];
 
 let cardsInPlay = [];
 
 function checkForMatch(){
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	  console.log("You found a match!");
+	  
 	}
 	 else 
 	{
@@ -15,18 +36,23 @@ function checkForMatch(){
 };
 
 function flipCard(cardId){
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
 
-	cardsInPlay.push(cards[cardId])
+	cardsInPlay.push(cards[cardId].rank);
 
 	console.log(cardsInPlay);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	
 	checkForMatch();
 	
+
 };
 flipCard(0);
 flipCard(2);
 
+//console.log(cardImage);
+//console.log(suit);
 
 
 
