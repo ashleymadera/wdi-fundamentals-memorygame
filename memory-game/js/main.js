8a8492321d15bd823e14c8cbb3b507b1962e51c3
Ashley Madera <ashleymadera@Ashleys-MacBook-Pro.local>
2020-03-17 15:34:43 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,34 +2,34 @@
 
 let cards = ["queen","queen","king","king"];
 
-let cardsInPlay=[];
+let cardsInPlay = [];
 
-let cardOne = cards[0];
-
-let cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-
-
-console.log(cardsInPlay);
-if (cardsInPlay.length === 2)
-{
-    if(cardsInPlay[0] === cardsInPlay[1])
-	{
-		alert("You found a match!");	
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	  console.log("You found a match!");
 	}
-	else
+	 else 
 	{
-	alert("Sorry, try again.");	
-	};
+	  console.log("Sorry, try again.");
+	}
 };
 
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+
+	cardsInPlay.push(cards[cardId])
+
+	console.log(cardsInPlay);
+	
+	checkForMatch();
+	
+};
+flipCard(0);
+flipCard(2);
 
 
-/*if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}else{
-		alert("Sorry, try again.");	
-	};*/
+
+
+
+
+
